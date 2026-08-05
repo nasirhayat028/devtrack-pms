d0ff0461b611f7ae8f8f1df6aabe30c59c2c0d0f
nasirhayat028 <nasirhayat.dev@gmail.com>
2026-08-05 17:02:28 +0500

    Update Image Tags in Minifest

--- scripts/update-manifests.sh
+++ scripts/update-manifests.sh
@@ -13,3 +13,20 @@ grep "image:" k8s/backend/deployment.yaml
 echo ""
 echo "Current Frontend Image:"
 grep "image:" k8s/frontend/deployment.yaml
+
+echo ""
+echo "Updating Backend Image..."
+
+sed -i "s|image: nasirhayat028/devtrack-backend:.*|image: nasirhayat028/devtrack-backend:$IMAGE_TAG|" k8s/backend/deployment.yaml
+
+echo "Updated Backend Image:"
+grep "image:" k8s/backend/deployment.yaml
+
+
+echo ""
+echo "Updating Frontend Image..."
+
+sed -i "s|image: nasirhayat028/devtrack-frontend:.*|image: nasirhayat028/devtrack-frontend:$IMAGE_TAG|" k8s/frontend/deployment.yaml
+
+echo "Updated Frontend Image:"
+grep "image:" k8s/frontend/deployment.yaml
